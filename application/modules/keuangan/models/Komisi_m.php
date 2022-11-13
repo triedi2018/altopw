d2321be00fe826e335472b75cd3aaeea9706f104
wahyusaputra202109 <wahyu.saputra202109@gmail.com>
2022-11-13 20:36:26 +0700

    Komisi

---
+++ application/modules/keuangan/models/Komisi_m.php
@@ -0,0 +1,207 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+
+class Komisi_m extends CI_Model {
+	
+	public function list_produk(){
+		return $this->db->get('data_produk')->result_array();
+	}	
+
+	public function edit(){
+		$data = $this->db
+		->select('u.*')
+		->from('komisi u')
+		->where(['md5(u.id)' => $this->input->post('id')])
+		->get()->row_array();
+		return $data;
+	}
+	public function simpan_tambah(){
+		// cek user exist
+		//$cek = $this->db->get_where('barang_masuk',['nama_produk' => $this->input->post('nama_produk')])->row_array();
+		//if ($cek){
+			//return json_encode(['status' => 'error','pesan' => 'Gagal menyimpan data, Nama Pelanggan sudah ada..']);
+		//}
+		// insert ke table user
+		$this->db->insert('komisi',
+		[
+			'tanggal' => convert_date_to_en($this->input->post('tanggal')),
+			'nama' => $this->input->post('nama'),
+			'jumlah' => $this->input->post('jumlah'),
+			'keterangan' => $this->input->post('keterangan'),
+			'created_at' => date('Y-m-d H:i:s')
+		]);
+
+		return json_encode(['status' => 'success','pesan' => 'Data berhasil disimpan']);
+
+	}
+	public function simpan_edit(){
+
+		$this->db->where('md5(id)',$this->input->post('id'))->update('komisi',
+		[
+			'tanggal' => convert_date_to_en($this->input->post('tanggal')),
+			'nama' => $this->input->post('nama'),
+			'jumlah' => $this->input->post('jumlah'),
+			'keterangan' => $this->input->post('keterangan'),
+		]);
+
+		if ($this->db->affected_rows() > 0){
+			return json_encode(['status' => 'success','pesan' => 'Data berhasil diperbaharui']);
+		}else{
+			return json_encode(['status' => 'success','pesan' => 'Tidak ada data yang diperbaharui']);
+		}
+	}
+	
+	public function hapus(){
+		$this->db
+		->where(['md5(u.id)' => $this->input->post('id')]);
+		$this->db->delete('komisi u');
+		return $this->db->affected_rows(); 
+	}
+
+    var $table = '( select * from komisi ) u';
+	var $column_order = array('','u.id'); //set order berdasarkan field yang di mau
+	var $column_search = array('u.nama','keterangan'); //set field yang bisa di search
+	var $order = array('u.id' => 'desc'); // default order 
+
+	private function _get_data()
+	{		
+        $this->db->select('u.*');
+		$this->db->from($this->table);
+		
+		$sql = " id is not null ";
+		
+		if ($this->input->post('view') != '') {
+			if($this->input->post('view') == 'all') {
+				if ($this->input->post('startdate') != '') {
+					$startdate = $this->input->post('startdate');
+					$sql .= " and tanggal > '$startdate 00:00:00' ";
+				}
+				if ($this->input->post('enddate') != '') {
+					$enddate = $this->input->post('enddate');
+					$sql .= " and tanggal < '$enddate 23:59:59' ";
+				}
+			}
+			else if($this->input->post('view') == 'DEBIT') {
+				$sql .= " and jenis_transaksi = 'DEBIT' " ;
+				if ($this->input->post('startdate') != '') {
+					$startdate = $this->input->post('startdate');
+					$sql .= " and tanggal > '$startdate 00:00:00' ";
+				}
+				if ($this->input->post('enddate') != '') {
+					$enddate = $this->input->post('enddate');
+					$sql .= " and tanggal < '$enddate 23:59:59' ";
+				}				
+				
+			}
+			else if($this->input->post('view') == 'KREDIT') {
+				$sql .= " and jenis_transaksi = 'KREDIT' " ;
+				if ($this->input->post('startdate') != '') {
+					$startdate = $this->input->post('startdate');
+					$sql .= " and tanggal > '$startdate 00:00:00' ";
+				}
+				if ($this->input->post('enddate') != '') {
+					$enddate = $this->input->post('enddate');
+					$sql .= " and tanggal < '$enddate 23:59:59' ";
+				}				
+			}
+			
+		}	
+		
+		$this->db->where($sql);		
+		
+		$i = 0;	
+		foreach ($this->column_search as $item) // loop column 
+		{
+			if($_POST['search']['value']) // cek kalo ada search data
+			{				
+				if($i===0) // first loop
+				{
+					$this->db->group_start(); // open group like or like
+					$this->db->like($item, $_POST['search']['value']);
+				}
+				else
+				{
+					$this->db->or_like($item, $_POST['search']['value']);
+				}
+
+				if(count($this->column_search) - 1 == $i) //last loop
+					$this->db->group_end(); //close group like or like
+			}
+			$i++;
+		}		
+		if(isset($_POST['order'])) // cek kalo click order
+		{
+			$this->db->order_by($this->column_order[$_POST['order']['0']['column']], $_POST['order']['0']['dir']);
+		} 
+		else if(isset($this->order))
+		{
+			$order = $this->order;
+			$this->db->order_by(key($order), $order[key($order)]);
+		}
+	}
+
+	function tampildata()
+	{
+		$this->_get_data();
+		if($_POST['length'] != -1)
+		$this->db->limit($_POST['length'], $_POST['start']);
+		$query = $this->db->get();
+		return $query->result_array();
+	}
+
+	function count_filtered()
+	{
+		$this->_get_data();
+		$query = $this->db->get();
+		return $query->num_rows();
+	}
+
+	public function count_all()
+	{
+		$this->db->from($this->table);
+		
+		$sql = " id is not null ";
+		
+		if ($this->input->post('view') != '') {
+			if($this->input->post('view') == 'all') {
+				if ($this->input->post('startdate') != '') {
+					$startdate = $this->input->post('startdate');
+					$sql .= " and tanggal > '$startdate 00:00:00' ";
+				}
+				if ($this->input->post('enddate') != '') {
+					$enddate = $this->input->post('enddate');
+					$sql .= " and tanggal < '$enddate 23:59:59' ";
+				}
+			}
+			else if($this->input->post('view') == 'DEBIT') {
+				$sql .= " and jenis_transaksi = 'DEBIT' " ;
+				if ($this->input->post('startdate') != '') {
+					$startdate = $this->input->post('startdate');
+					$sql .= " and tanggal > '$startdate 00:00:00' ";
+				}
+				if ($this->input->post('enddate') != '') {
+					$enddate = $this->input->post('enddate');
+					$sql .= " and tanggal < '$enddate 23:59:59' ";
+				}				
+				
+			}
+			else if($this->input->post('view') == 'KREDIT') {
+				$sql .= " and jenis_transaksi = 'KREDIT' " ;
+				if ($this->input->post('startdate') != '') {
+					$startdate = $this->input->post('startdate');
+					$sql .= " and tanggal > '$startdate 00:00:00' ";
+				}
+				if ($this->input->post('enddate') != '') {
+					$enddate = $this->input->post('enddate');
+					$sql .= " and tanggal < '$enddate 23:59:59' ";
+				}				
+			}
+			
+		}	
+		
+		$this->db->where($sql);			
+		
+		return $this->db->count_all_results();
+	}
+}
---
+++ application/modules/keuangan/views/komisi_js.php
@@ -0,0 +1,439 @@
+<script src="https://cdn.datatables.net/buttons/2.1.0/js/dataTables.buttons.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.1.0/js/buttons.html5.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.1.0/js/buttons.print.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.1.0/js/buttons.colVis.min.js"></script>
+
+<script src="<?php echo base_url(); ?>bower_components/moment/min/moment.min.js"></script>
+<script src="<?php echo base_url(); ?>bower_components/bootstrap-daterangepicker/daterangepicker.js"></script>
+
+<script type="text/javascript">
+
+//set default swal sweet alert..
+const Toast = Swal.mixin({
+      toast: true,
+      position: 'top-end',
+      showConfirmButton: false,
+      timer: 3000
+});
+
+
+$('#reservation').daterangepicker(
+{
+	startDate : moment().startOf('years'),
+	locale: {
+		format: 'DD/MM/YYYY'
+	}        
+})
+
+$('#reservation').on('apply.daterangepicker', function(ev, picker) {
+	
+	table_data.ajax.reload();
+
+})
+
+$('.btn-action').on('click',function(){
+  show_loading();
+
+  xhr = $.ajax({
+    method : "POST",
+    url : "<?= base_url().$this->uri->segment(1,0).$this->uri->slash_segment(2,'both')?>tambah",
+    data:"jenis=tambah",
+    success: function(response){
+      $('.tampil-modal').html(response);
+      $('.modal-action').modal({
+        backdrop: 'static',
+        keyboard: false},'show');
+
+        validate_form('tambah');
+        cek_divisi_jabatan();
+        call_datepicker();
+		
+		  $(".allow_only_numbers").keydown(function(e) {
+
+			// Allow: backspace, delete, tab, escape, enter and .
+			if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 173 , 190]) !== -1 ||
+			  // Allow: Ctrl+A,Ctrl+C,Ctrl+V, Command+A
+			  ((e.keyCode == 65 || e.keyCode == 86 || e.keyCode == 67) && (e.metaKey === true)) ||
+			  // Allow: home, end, left, right, down, up
+			  (e.keyCode >= 35 && e.keyCode <= 40)) {
+			  // let it happen, don't do anything
+			  return;
+			}
+			// Ensure that it is a number and stop the keypress
+			if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {			
+			  e.preventDefault();
+			}
+		  });
+
+	xhr = $.ajax({
+	  method : "POST",
+	  url : "<?= base_url().$this->uri->segment(1,0).$this->uri->slash_segment(2,'both')?>list-produk",
+	  success: function(response){
+		$('#list_produk').html(response);
+				
+		hide_loading();
+	  },
+	  error : function(){
+
+	  }
+	})
+		  
+
+      hide_loading();
+    },
+    error : function(){
+
+    }
+  })
+})
+function cek_divisi_jabatan(){
+  $('#form-action').on('change','#divisi',function(e){
+    // console.log(e.target.value);
+    show_loading();
+    xhr = $.ajax({
+      method : "POST",
+      url : "<?= base_url().$this->uri->segment(1,0).$this->uri->slash_segment(2,'both')?>list-jabatan",
+      data : "divisi="+e.target.value,
+      success: function(response){
+        $('#jabatan').html(response);
+        hide_loading();
+      },
+      error : function(){
+
+      }
+    })
+    
+  });
+  $('#form-action').on('change','#jabatan',function(e){
+    if (e.target.value != "" ){
+      // console.log(e.target.value);
+      show_loading();
+      xhr = $.ajax({
+        method : "POST",
+        url : "<?= base_url().$this->uri->segment(1,0).$this->uri->slash_segment(2,'both')?>list-atasan",
+        data : "nik="+$('#nik').val()+"&divisi="+$('#divisi').val(),
+        success: function(response){
+          $('#atasan1').html(response);
+          $('#atasan2').html(response);
+          hide_loading();
+        },
+        error : function(){
+
+        }
+      })
+    }
+  });
+}
+function validate_form(action){
+  
+
+  $.validator.setDefaults({
+    
+  });
+  
+  $('#form-action').validate({
+    rules: {
+      tanggal:{required:true},
+      nama: { required: true },
+      keterangan:{required:true},
+	  jumlah:{required:true},
+    },
+    messages: {
+      tanggal:"Harus diisi",
+      nama:"Harus diisi",
+      keterangan:"Harus diisi",
+	  jumlah:"Harus diisi"
+    },
+    errorElement: 'span',
+    errorPlacement: function (error, element) {
+      error.addClass('invalid-feedback');
+
+        error.appendTo($(element).closest('.form-group').find('.symbol'));
+    },
+    highlight: function (element, errorClass, validClass) {
+    //   $(element).addClass('is-invalid');
+      $(element).closest('.form-group').removeClass('has-success').addClass('has-error').find('.symbol').removeClass('ok').addClass('required');
+
+    },
+    unhighlight: function (element, errorClass, validClass) {
+        $(element).closest('.form-group').removeClass('has-error');
+
+    },
+    success: function (label, element) {
+        label.addClass('help-block valid');
+        // mark the current input as valid and display OK icon
+        $(element).closest('.form-group').removeClass('has-error').addClass('has-success').find('.symbol').removeClass('required').addClass('ok');
+    },
+    submitHandler: function () {
+      $('#simpan').text('Menyimpan data...');
+      $('#simpan').attr('disabled','disabled');
+
+      show_loading();
+      let formdata = $('#form-action').serialize();
+      xhr = $.ajax({
+        method : "POST",
+        url : "<?= base_url().$this->uri->segment(1,0).$this->uri->slash_segment(2,'both') ?>simpan-"+action,
+        data : formdata,
+        success: function(response){
+          let result = JSON.parse(response);
+
+          if (result.status == 'error'){
+            hide_loading();
+            $('#simpan').text('Simpan');
+            $('#simpan').removeAttr('disabled');
+          }else{
+            reload_table();
+            $('.modal-action').modal('hide');
+            hide_loading();
+          }
+          Toast.fire({
+            type: result.status,
+            title: result.pesan
+          })
+          
+
+        },
+        
+
+      })
+
+
+
+    }
+  });
+}
+
+
+function detail_table ( d ) {
+    return d.action;
+}
+$(document).ready(function () {
+    table_data = $('#example1').DataTable({
+
+		dom: 'Bfrtip',
+		buttons: [ {
+                extend: 'excel',
+                //orientation: 'landscape',
+                //pageSize: 'LEGAL',
+                exportOptions: {
+                    //columns: ':visible'
+					columns: [ 1, 2, 3, 4 ],
+					stripHtml: true,
+                },
+		action: newexportaction_all				
+            },            {
+                extend: 'pdfHtml5',
+                //orientation: 'landscape',
+                //pageSize: 'LEGAL',
+                exportOptions: {
+                    //columns: ':visible'
+					columns: [ 1, 2, 3, 4 ],
+					stripHtml: true,
+                },
+		action: newexportaction_all				
+            } ],		
+		
+        "processing": true, //Feature control the processing indicator.
+        "serverSide": true, //Feature control DataTables' server-side processing mode.
+        "order": [], //Initial no order.
+
+        // Load data for the table's content from an Ajax source
+        "ajax": {
+            "url": "<?= base_url().$this->uri->segment(1,0).$this->uri->slash_segment(2,'leading')."/tampildata"; ?>",
+            "type": "POST",
+            "data":{'<?= $this->security->get_csrf_token_name() ?>':'<?= $this->security->get_csrf_hash() ?>',
+				//view: function() { return $('#view').val() },
+				  startdate: function() { return $('#reservation').data('daterangepicker').startDate.format('YYYY-MM-DD') },
+				  enddate: function() { return $('#reservation').data('daterangepicker').endDate.format('YYYY-MM-DD') },			
+			},
+            
+        },
+
+        //Set column definition initialisation properties.
+        // "columnDefs": [
+        // { 
+        //     "targets": [ 5 ], //last column
+        //     "orderable": false, //set not orderable
+        // }
+        // ],
+        
+
+        "columns": [
+            {
+                "class":          "details-control",
+                "orderable":      false,
+                "data":           null,
+                "defaultContent": ""
+            },
+            { "data": "tanggal" },
+            { "data": "nama" },
+            { "data": "jumlah" },
+			{ "data": "keterangan" },
+        ],
+		
+		"lengthMenu": [[50, -1], [50, "All"]]
+
+    });
+    var detailRows = [];
+    $('#example1 tbody').on( 'click', 'tr td.details-control', function () {
+        var tr = $(this).closest('tr');
+        var row = table_data.row( tr );
+        var idx = $.inArray( tr.attr('id'), detailRows );
+ 
+        if ( row.child.isShown() ) {
+            tr.removeClass( 'details' );
+            row.child.hide();
+ 
+            // Remove from the 'open' array
+            detailRows.splice( idx, 1 );
+        }
+        else {
+            tr.addClass( 'details' );
+            row.child( detail_table( row.data() ) ).show();
+ 
+            // Add to the 'open' array
+            if ( idx === -1 ) {
+                detailRows.push( tr.attr('id') );
+            }
+        }
+    } );
+
+    // action edit dan delete
+    $('#example1 tbody').on('click','.btn-edit',function(e){
+      // alert(e.data);
+      // console.log('nik',e.target.dataset.nik);
+      // console.log('csrf',e.target.dataset.<?=$this->security->get_csrf_token_name() ?>);
+      // console.log('id',e.target.dataset.id);
+      // console.log('jenis action',e.target.dataset.jenis_action);
+
+      xhr = $.ajax({
+        method : "POST",
+        url : "<?= base_url().$this->uri->segment(1,0).$this->uri->slash_segment(2,'both')?>edit",
+        data: {"jenis":"edit","nik":e.target.dataset.nik,'id': e.target.dataset.id,'<?= $this->security->get_csrf_token_name() ?>':e.target.dataset.<?=$this->security->get_csrf_token_name() ?> },
+        success: function(response){
+          if (response == 'error'){
+            window.location.href = "<?= base_url('unauthorized') ?>";
+          }else{
+            $('.tampil-modal').html(response);
+            $('.modal-action').modal({
+              backdrop: 'static',
+              keyboard: false},'show');
+
+              validate_form('edit');
+              cek_divisi_jabatan();
+              call_datepicker();
+
+          }
+
+          hide_loading();
+        },
+        error : function(){
+
+        }
+      })
+
+    })
+	
+    $('#example1 tbody').on('click','.btn-hapus',function(e){
+      console.log(e);
+
+		Swal.fire({
+			title: "Hapus data ini?",
+			text: "Data yang terhapus tidak bisa dikembalikan!",
+			  icon: 'warning',
+			  showCancelButton: true,
+			  timerProgressBar: true,
+			  confirmButtonColor: '#3085d6',
+			  cancelButtonColor: '#d33',
+			  confirmButtonText: "Iya, Hapus",
+		}).then((result) => {
+
+		if (result.isConfirmed) {
+
+		      xhr = $.ajax({
+			method : "POST",
+			url : "<?= base_url().$this->uri->segment(1,0).$this->uri->slash_segment(2,'both')?>hapus",
+			data: {"jenis":"delete","id":e.target.dataset.id,'<?= $this->security->get_csrf_token_name() ?>':e.target.dataset.<?=$this->security->get_csrf_token_name() ?> },
+			success: function(response){
+			  if (response == 'error'){
+			    window.location.href = "<?= base_url('unauthorized') ?>";
+			  }else{
+			
+				reload_table();
+				
+			  }
+
+			  hide_loading();
+			},
+			error : function(){
+
+			}
+		      })
+
+		}			
+			
+		})
+
+    })	
+
+ 
+    // On each draw, loop over the `detailRows` array and show any child rows
+    // table_data.on( 'draw', function () {
+    //     $.each( detailRows, function ( i, id ) {
+    //         $('#'+id+' td.details-control').trigger( 'click' );
+    //     } );
+    // } );
+
+  
+});
+function reload_table()
+{
+  table_data.ajax.reload(null,false); //reload datatable ajax 
+}
+
+function newexportaction_all(e, dt, button, config) {
+    var self = this;
+    var oldStart = dt.settings()[0]._iDisplayStart;
+    dt.one('preXhr', function (e, s, data) {
+        // Just this once, load all data from the server...
+        data.start = 0;
+        data.length = 2147483647;
+        dt.one('preDraw', function (e, settings) {
+            // Call the original action function
+            if (button[0].className.indexOf('buttons-copy') >= 0) {
+                $.fn.dataTable.ext.buttons.copyHtml5.action.call(self, e, dt, button, config);
+            } else if (button[0].className.indexOf('buttons-excel') >= 0) {
+                $.fn.dataTable.ext.buttons.excelHtml5.available(dt, config) ?
+                    $.fn.dataTable.ext.buttons.excelHtml5.action.call(self, e, dt, button, config) :
+                    $.fn.dataTable.ext.buttons.excelFlash.action.call(self, e, dt, button, config);
+            } else if (button[0].className.indexOf('buttons-csv') >= 0) {
+                $.fn.dataTable.ext.buttons.csvHtml5.available(dt, config) ?
+                    $.fn.dataTable.ext.buttons.csvHtml5.action.call(self, e, dt, button, config) :
+                    $.fn.dataTable.ext.buttons.csvFlash.action.call(self, e, dt, button, config);
+            } else if (button[0].className.indexOf('buttons-pdf') >= 0) {
+                $.fn.dataTable.ext.buttons.pdfHtml5.available(dt, config) ?
+                    $.fn.dataTable.ext.buttons.pdfHtml5.action.call(self, e, dt, button, config) :
+                    $.fn.dataTable.ext.buttons.pdfFlash.action.call(self, e, dt, button, config);
+            } else if (button[0].className.indexOf('buttons-print') >= 0) {
+                $.fn.dataTable.ext.buttons.print.action(e, dt, button, config);
+            }
+            dt.one('preXhr', function (e, s, data) {
+                // DataTables thinks the first item displayed is index 0, but we're not drawing that.
+                // Set the property to what it was before exporting.
+                settings._iDisplayStart = oldStart;
+                data.start = oldStart;
+            });
+            // Reload the grid with the original page. Otherwise, API functions like table.cell(this) don't work properly.
+            setTimeout(dt.ajax.reload, 0);
+            // Prevent rendering of the full data to the DOM
+            return false;
+        });
+    });
+    // Requery the server with the new one-time export settings
+    dt.ajax.reload();
+};
+
+</script>
